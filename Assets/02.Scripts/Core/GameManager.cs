5857ecbe37981750e19e26835859d46f2cf10935
Crogen0407 <mymcoding0407@gmail.com>
2024-02-23 22:03:50 +0900

    feat :MonoSingleton

---
+++ Assets/02.Scripts/Core/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public Player Player { get; private set; }
+    
+    private void Awake()
+    {
+        Player = FindObjectOfType<Player>();
+    }
+}
