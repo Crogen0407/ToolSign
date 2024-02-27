3ed8e9b8572248b5dfe3ae094a3017372f36153c
Crogen0407 <mymcoding0407@gmail.com>
2024-02-28 00:28:06 +0900

    feat : PlayerMove

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Vector3 _colliderCenter;
+    [SerializeField] private LayerMask _layerMask;
+    
     public InputReader inputReader;
     public CameraConverter cameraConverter;
     [SerializeField] private float _moveSpeed = 5;
@@ -25,9 +28,17 @@ public class Player : MonoBehaviour
     {
         Vector3 vec = new Vector3(cameraConverter.CurrentCameraController.ForwardVector.x, 0, cameraConverter.CurrentCameraController.ForwardVector.z).normalized;
         vec = (vec * inputReader.InputVector.z) + (cameraConverter.CurrentCameraController.RightVector * inputReader.InputVector.x);
-        _rigidbody.velocity = vec * _moveSpeed;
+        if(CheckAroundCollider(_colliderCenter, vec, 1.2f, _layerMask) == false)
+            _rigidbody.velocity = vec * _moveSpeed;
     }
-    
+
+    private bool CheckAroundCollider(Vector3 center, Vector3 direction, float maxDistance, LayerMask layerMask)
+    {
+        center += transform.position;
+        Debug.DrawRay(center, direction * maxDistance, Color.green, 0.1f);
+        return Physics.Raycast(center, direction, maxDistance, layerMask);
+    }
+
     void Update()
     {
         Move();
