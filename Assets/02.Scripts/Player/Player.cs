5f13f0cbbd6d824e5ad1321ab49a2735070d1e79
Crogen0407 <mymcoding0407@gmail.com>
2024-02-29 00:38:11 +0900

    feat : Move

--- Assets/02.Scripts/Player/Player.cs
+++ Assets/02.Scripts/Player/Player.cs
@@ -28,17 +28,29 @@ public class Player : MonoBehaviour
     {
         Vector3 vec = new Vector3(cameraConverter.CurrentCameraController.ForwardVector.x, 0, cameraConverter.CurrentCameraController.ForwardVector.z).normalized;
         vec = (vec * inputReader.InputVector.z) + (cameraConverter.CurrentCameraController.RightVector * inputReader.InputVector.x);
+
+        float X = (int)Mathf.Clamp(vec.x, -1, 1);
+        float Z = (int)Mathf.Clamp(vec.z, -1, 1);
+        
         _rigidbody.velocity = new Vector3(
-            CheckAroundCollider(_colliderCenter, new Vector3(vec.x, 0, 0), 1.2f, _layerMask),
+            CheckAroundCollider(_colliderCenter, new Vector3(X, 0, 0), X * 1.2f, _layerMask),
             0, 
-            CheckAroundCollider(_colliderCenter, new Vector3(0, 0, vec.z), 1.2f, _layerMask)) * _moveSpeed;
+            CheckAroundCollider(_colliderCenter, new Vector3(0, 0, Z), Z * 1.2f, _layerMask)) * _moveSpeed;
+        
+        //Debug.Log($"{CheckAroundCollider(_colliderCenter, new Vector3(vec.x, 0, 0), (int)Mathf.Clamp(vec.x, -1, 1) * 1.2f, _layerMask)} {CheckAroundCollider(_colliderCenter, new Vector3(0, 0, vec.z),  (int)Mathf.Clamp(vec.z, -1, 1) * 1.2f, _layerMask)}");
     }
 
-    private int CheckAroundCollider(Vector3 center, Vector3 direction, float maxDistance, LayerMask layerMask)
+    private float CheckAroundCollider(Vector3 center, Vector3 direction, float maxDistance, LayerMask layerMask)
     {
         center += transform.position;
-        Debug.DrawRay(center, direction * maxDistance, Color.green, 0.1f);
-        return Physics.Raycast(center, direction, maxDistance, layerMask) == true || direction == Vector3.zero ? 0 : 1;
+        if (maxDistance != 0)
+        {
+            Debug.DrawRay(center, direction * Mathf.Abs(maxDistance), Color.green, 0.1f);
+            Debug.Log(maxDistance / Mathf.Abs(maxDistance));
+            return maxDistance / Mathf.Abs(maxDistance);
+        }
+
+        return 0;
     }
 
     void Update()
